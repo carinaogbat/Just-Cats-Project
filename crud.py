52bf0178393caefdb0456cf82554def40ed38056
Carina Ogbat <carinawrightogbat@gmail.com>
2022-11-03 20:40:23 -0700

    added files

---
+++ crud.py
@@ -0,0 +1,25 @@
+
+
+
+
+# create a user 
+
+# return a user
+
+# upload a photo
+
+# view a photo
+
+# delete a photo
+
+#create a like
+
+#create a comment
+
+#create a notification
+
+#return a notification
+
+#create a notification comment
+
+#return a notification comment
