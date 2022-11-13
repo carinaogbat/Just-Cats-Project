9774eaf16422cbb5d0ea5fa4792673a6b1770425
Carina Ogbat <carinawrightogbat@gmail.com>
2022-11-12 16:10:14 -0800

    cleared out hashrouting

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -108,39 +108,7 @@ function Hello(props) {
     )
 }
 
-// const Login = () => <h1>Login</h1>
-// const SignUp = () => <h1>Sign Up </h1>
-// const MyProfile = () => <h1>My Profile</h1>
-// const UserProfile = () => <h1>Other User Profile</h1>
-// const MyPhoto = () => <h1>My cute photo</h1>
-// const UserPhoto = () => <h1>another person's cute photo</h1>
-// const Following = () => <h1>Users Followed</h1>
 
-
-// const App = () => (
-//     <ReactRouterDOM.HashRouter>
-//       <ul>
-//         <li><Link to="/">Home</Link></li>
-//         <li><Link to="/login">Login</Link></li>
-//         <li><Link to="/signup">Sign Up</Link></li>
-//         <li><Link to="/myprofle">My Profile</Link></li>
-//         <li><Link to="/userprofile">User Profle</Link></li>
-//         <li><Link path="/myphoto">My Photo</Link></li>
-//         <li><Link path="/userphoto">User photo</Link></li>
-//         <li><Link path="/following">Following</Link></li>
-//       </ul>
-  
-//       <Route path="/" exact component={Home} />
-//       <Route path="/login" component={Login} />
-//       <Route path="/signUp" component={SignUp} />
-//       <Route path="/myprofile" component={MyProfile} />
-//       <Route path="/userprofile" component={UserProfile} />
-//       <Route path="/myphoto" component={MyPhoto} />
-//       <Route path="/userphoto" component={UserPhoto} />
-//       <Route path="/following" component={Following} />
-      
-//     </ReactRouterDOM.HashRouter>
-//   )
 
 function App() {
     return (
